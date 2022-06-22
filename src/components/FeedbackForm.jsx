f8042e29368f1d410a76bab19472bcd7fc7c1ee7
Juan D Chaves <juandavidchaves@protonmail.com>
2022-06-22 14:14:56 -0300

    added Form, Stats

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,14 @@
+import Card from "./shared/Card"
+
+function FeedbackForm() {
+  return (
+    <Card>
+        <form>
+            <h2>How would you rate our service?</h2>
+        </form>
+
+    </Card>
+  )
+}
+
+export default FeedbackForm
